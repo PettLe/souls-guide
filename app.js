7a5302560b46df431ebbde3029815d6bdcdd1474
PettLe <petewaterz@gmail.com>
2022-01-25 10:23:37 +0200

    added countdown timer for Elden Ring release

--- app.js
+++ app.js
@@ -39,3 +39,31 @@ const feedback = document.getElementById("feedback");
 feedback.addEventListener("click", function () {
   alert("Nothing here yet, sorry!");
 });
+
+var countDownDate = new Date("Feb 25, 2022 00:00:01").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  const timer = document.getElementById("timer");
+  timer.innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "EXPIRED";
+  }
+}, 1000);
